e9b2a5299868b8189fe12d7fe8dae062e62afcee
pradipta Kumar Das <pradipta.das115@gmail.com>
2019-10-15 22:27:47 +0530

    first commit

---
+++ actions/greet.py
@@ -0,0 +1,9 @@
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+    def run(self, message):
+        print(message)
+
+        if message == 'working':
+            return (True, message)
+        return (False, message)
